2073c799e6be47da594837cff27d71f64fa623ad
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-10-08 11:24:20 +0600

    V-1

---
+++ lib/constant/url.dart
@@ -0,0 +1,10 @@
+
+class URL {
+  static const facebookURL = "https://www.facebook.com/vipmehedi/";
+  static const linkedinURL = "https://www.linkedin.com/in/jpmehedi/";
+  static const githubURL = "https://www.github.com/jpmehedi/";
+  static const stackoverflowURL = "https://stackoverflow.com/users/12755955/mehedi-hasan/";
+  static const blogURL = "https://mehedihasanjp.blogspot.com/";
+  static const youtubeURL = "https://youtube.com/goflutter/";
+  static const resumeURL = "https://drive.google.com/file/d/1mft1s4ZdLFOrM0FBetw87QvPGZ3vwdMC/view?usp=sharing";
+}
---
+++ lib/screens/body/about/data.dart
@@ -0,0 +1,12 @@
+List<Map<dynamic, dynamic>> addressInfo = [
+  {"level": "Age", "data": "21"},
+  {"level": "Email", "data": "hasannahidnazmul@gmail.com"},
+  {"level": "Phone", "data": "+8801 737 185 373"},
+  {"level": "Address", "data": "66, Green Road, Dhaka- 1205"},
+  {"level": "Language", "data": "English, Bangla"}
+];
+
+String about = """
+I’m Nuzmul Hossain Nahid. I passionate about Cross-platform Mobile application development (Flutter). I have 1.5 year experience in flutter development. I’m constantly striving to learn new things and improve. I follow industry news; regularly tinker with new technologies, and read books and blogs to keep my skills current. I am completed graduation in Computer Science & Engineering. I have great skills in Flutter, Dart, HTML, CSS, Bootstrap framework, and also have expertise in Word Press.I have a keen knowledge of Firebase, SQLite database management system. I am also has great experience in UX/UI designing Mobile Application
+
+""";
